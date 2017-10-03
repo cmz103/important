aac09aa9cd714b06fcd17694591ccb003dd801a0
Cedric Maniquiz <cgmaniquiz@gmail.com>
2017-10-02 22:00:49 -0700

    update app.ts notification init

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,2 @@
-let imp = new Important(<HTMLElement>document.getElementById("impWrapper"));
+// import { Important } from "./important";
+const imp = new Important(document.getElementById("impWrapper") as HTMLElement);
